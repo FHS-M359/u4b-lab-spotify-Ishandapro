7d358fb6ff42815ab98737c714e634fdbc1729c3
Ishan G <probloxguy20@gmail.com>
2026-02-24 22:51:05 -0600

    Did basic tester for printing txt file out

--- U4BLab/src/SpotifyTester.java
+++ U4BLab/src/SpotifyTester.java
@@ -1,2 +1,8 @@
 public class SpotifyTester {
+  public static void main(String[] args)
+  {
+    Playlist p = new Playlist();
+    p.readFile("spotify_unique_years_artists.txt");
+    System.out.println(p);
+  }
 }
