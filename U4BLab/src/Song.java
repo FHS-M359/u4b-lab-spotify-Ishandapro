37b1768c568fc2525e78ba763310cdac5f0aa79b
Ishan G <probloxguy20@gmail.com>
2026-02-24 22:25:32 -0600

    Finished Basic stuff for Song.java

--- U4BLab/src/Song.java
+++ U4BLab/src/Song.java
@@ -1,2 +1,58 @@
-public class Song {
+public class Song
+{
+    private String title;
+    private String artist;
+    private String album;
+    private int durationSeconds;
+    private int releaseYear;
+    private String genre;
+
+    // Constructor
+    public Song(String t, String ar, String al, int d, int y, String g)
+    {
+        title = t;
+        artist = ar;
+        album = al;
+        durationSeconds = d;
+        releaseYear = y;
+        genre = g;
+    }
+
+    // Getters
+    public String getTitle()
+    {
+        return title;
+    }
+
+    public String getArtist()
+    {
+        return artist;
+    }
+
+    public String getAlbum()
+    {
+        return album;
+    }
+
+    public int getDurationSeconds()
+    {
+        return durationSeconds;
+    }
+
+    public int getReleaseYear()
+    {
+        return releaseYear;
+    }
+
+    public String getGenre()
+    {
+        return genre;
+    }
+
+    // toString (one row format)
+    public String toString()
+    {
+        return String.format("%-30s %-20s %-25s %-6d %-15s",
+                title, artist, album, releaseYear, genre);
+    }
 }
